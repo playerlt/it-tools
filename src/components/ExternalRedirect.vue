569b2e2f148cfabe8a73302a78befae39678f980
player <3127321495@qq.com>
2024-06-25 01:32:17 +0800

    feat: 修改跳转链接

--- src/components/ExternalRedirect.vue
+++ src/components/ExternalRedirect.vue
@@ -5,7 +5,24 @@
 <script>
 export default {
   mounted() {
+    // 清除特定的 cookie
+    document.cookie = "cookieName=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    // 或清除所有 cookie
+    this.clearAllCookies();
+
+    // 重定向操作
     window.location.href = this.$route.meta.url;
+  },
+  methods: {
+    clearAllCookies() {
+      const cookies = document.cookie.split(";");
+
+      for (let cookie of cookies) {
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+      }
+    }
   }
 }
 </script>
